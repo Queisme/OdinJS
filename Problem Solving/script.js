dd4d78aebe74e1ef94f95254832c9691af9efdcf
Queisme <queiskey@gmail.com>
2024-07-18 15:08:52 -0500

    FizzBuzz

--- Problem Solving/script.js
+++ Problem Solving/script.js
@@ -1,12 +1,7 @@
 let number = +prompt('Give us a number:');
 for (i = 1; i <= number; i++) {
-  let output = i;
-  if (i % 15 == 0) {
-    output = 'FizzBuzz';
-  } else if (i % 5 == 0) {
-    output = 'Buzz';
-  } else if (i % 3 == 0) {
-    output = 'Fizz';
-  }
-  console.log(output);
+  let output = '';
+  if (i % 3 == 0) output += 'Fizz';
+  if (i % 5 == 0) output += 'Buzz';
+  console.log(output || i);
 }
